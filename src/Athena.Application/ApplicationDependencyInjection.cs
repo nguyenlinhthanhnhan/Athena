0f8abebf1522c011972c16506a98f51171c350f0
Nguyen Linh Thanh Nhan <nguyenlinhthanhnhan@outlook.com>
2023-05-24 22:20:54 +0700

    Remove FE

--- src/Athena.Application/ApplicationDependencyInjection.cs
+++ src/Athena.Application/ApplicationDependencyInjection.cs
@@ -1,5 +1,22 @@
-﻿namespace Athena.Application;
+﻿using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-public class ApplicationDependencyInjection
+namespace Athena.Application;
+
+public static class ApplicationDependencyInjection
 {
+    public static IServiceCollection AddApplication(this IServiceCollection serviceCollection,
+        IConfiguration configuration)
+    {
+        serviceCollection.AddAutoMapper(Assembly.GetExecutingAssembly());
+        serviceCollection.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        serviceCollection.AddMediatR(configs =>
+        {
+            configs.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+        });
+
+        return serviceCollection;
+    }
 }
