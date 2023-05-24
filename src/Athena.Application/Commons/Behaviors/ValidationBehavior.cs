745dce1238a64c2246d273e6713aacc1c52e6ac6
Nguyen Linh Thanh Nhan <nguyenlinhthanhnhan@outlook.com>
2023-05-25 01:39:01 +0700

    Fix vulnerable lib

--- src/Athena.Application/Commons/Behaviors/ValidationBehavior.cs
+++ src/Athena.Application/Commons/Behaviors/ValidationBehavior.cs
@@ -5,7 +5,7 @@ using MediatR;
 
 namespace Athena.Application.Commons.Behaviors;
 
-public class ValidationBehavior<TRequest, TResponse>:IPipelineBehavior<TRequest, TResponse>
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
 {
     private readonly IEnumerable<IValidator<TRequest>> _validators;
 
@@ -13,13 +13,15 @@ public class ValidationBehavior<TRequest, TResponse>:IPipelineBehavior<TRequest,
     {
         _validators = validators;
     }
-        
-    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
     {
         if (!_validators.Any()) return await next();
 
         var context = new ValidationContext<TRequest>(request);
-        var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+        var validationResults =
+            await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
         var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
 
         if (failures.Count != 0)
